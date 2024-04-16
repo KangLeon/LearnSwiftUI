ed7323efdecd3a95d04f5ae9f4425fb13c3ab1be
JY <397879704@qq.com>
2024-04-16 19:59:33 +0800

    new

---
+++ LearnSwiftUI/LearnSwiftUI/LearnSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftUIApp.swift
+//  LearnSwiftUI
+//
+//  Created by JY on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
