bc6a5d304db14f3e9f781899218717dafa312ba7
JY <397879704@qq.com>
2024-04-18 09:42:35 +0800

    new

---
+++ LearnSwiftUI/LearnSwiftUI/AlternativeConentView.swift
@@ -0,0 +1,18 @@
+//
+//  AlternativeConentView.swift
+//  LearnSwiftUI
+//
+//  Created by JY on 4/16/24.
+//
+
+import SwiftUI
+
+struct AlternativeConentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlternativeConentView()
+}
